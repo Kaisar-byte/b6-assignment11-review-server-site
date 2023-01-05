8e8303aa30e17551fd01ee28f4344353b3dc5e2e
Mohammad Kaisar Hamid <mohammadkaisarhamid@gmail.com>
2023-01-05 12:11:17 +0600

    see all btn implemented

--- index.js
+++ index.js
@@ -23,10 +23,15 @@ async function run() {
     app.get("/services", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
-      const services = await cursor.toArray();
+      const services = await cursor.limit(3).toArray();
       res.send(services);
     });
-
+    app.get("/services/seeall", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const seeall = await cursor.toArray();
+      res.send(seeall);
+    });
     app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
