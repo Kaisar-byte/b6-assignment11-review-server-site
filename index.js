b1b0e5e450dc64bf2b460cbb6b1b143524f51472
Kaisar-byte <55149528+Kaisar-byte@users.noreply.github.com>
2023-01-10 22:18:51 +0600

    Assignment project finally ready for submission

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
@@ -11,62 +12,112 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.i2wvmgk.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	serverApi: ServerApiVersion.v1,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    res.status(401).send({ message: "unauthorized access" });
+  }
+
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      res.status(401).send({ message: "authorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 async function run() {
-	try {
-		const serviceCollection = client.db("dentistJishan").collection("services");
-		const reviewCollection = client
-			.db("dentistJishan")
-			.collection("allreviews");
+  try {
+    const serviceCollection = client.db("dentistJishan").collection("services");
+    const reviewCollection = client
+      .db("dentistJishan")
+      .collection("allreviews");
 
-		app.get("/services", async (req, res) => {
-			const query = {};
-			const cursor = serviceCollection.find(query);
-			const services = await cursor.limit(3).toArray();
-			res.send(services);
-		});
-		app.get("/services/seeall", async (req, res) => {
-			const query = {};
-			const cursor = serviceCollection.find(query);
-			const seeall = await cursor.toArray();
-			res.send(seeall);
-		});
-		app.get("/services/:id", async (req, res) => {
-			const id = req.params.id;
-			const query = { _id: ObjectId(id) };
-			const service = await serviceCollection.findOne(query);
-			res.send(service);
-		});
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+    app.post("/addservice", async (req, res) => {
+      const service = req.body;
+      console.log(service);
+      const result = await serviceCollection.insertOne(service);
+      console.log(result);
+    });
 
-		app.get("/reviews", async (req, res) => {
-			let query = {};
-			if (req.query.email) {
-				query = {
-					email: req.query.email,
-				};
-			}
-			const cursor = reviewCollection.find(query);
-			const reviews = await cursor.toArray();
-			res.send(reviews);
-		});
-		app.post("/reviews", async (req, res) => {
-			const review = req.body;
-			const result = await reviewCollection.insertOne(review);
-			res.send(result);
-		});
-	} finally {
-	}
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.limit(3).toArray();
+      res.send(services);
+    });
+    app.get("/services/seeall", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const seeall = await cursor.toArray();
+      res.send(seeall);
+    });
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+
+    app.get("/reviews", verifyJWT, async (req, res) => {
+      //   console.log(req.headers.authorization);
+      let query = {};
+      if (req.query.email) {
+        query = {
+          email: req.query.email,
+        };
+      }
+      const cursor = reviewCollection.find(query);
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
+    app.post("/reviews", async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      res.send(result);
+    });
+
+    app.patch("/reviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const status = req.body.status;
+      const query = { _id: ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          status: status,
+        },
+      };
+      const result = await reviewCollection.updateOne(query, updatedDoc);
+      res.send(result);
+    });
+
+    app.delete("/reviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await reviewCollection.deleteOne(query);
+      res.send(result);
+    });
+  } finally {
+  }
 }
 run().catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
-	res.send("assignment 11 project server is running");
+  res.send("assignment 11 project server is running");
 });
 
 app.listen(port, () => {
-	console.log(`assignment 11 project server is running on port ${port}`);
+  console.log(`assignment 11 project server is running on port ${port}`);
 });
